a3839ab88e4e4b61a80ccbe25c214342b6130732
Yensi Julieth <yensi_96@misena.edu.co>
2015-07-07 07:15:23 -0500

    Creacion del crud ConfigUsuario y formulario de AddConfigUsuario

---
+++ capitulo5/PlantillaSQLite/crudConfigUsuaurio.php
@@ -0,0 +1,121 @@
+<?php
+	session_start();
+	require_once("install.php");
+	/* REQUEST = $_POST $_GET */
+	if (!empty($_REQUEST['action'])){
+		$accion = $_REQUEST['action'];
+		if($accion == 'crear'){
+			crearConfigUsuario();
+		}else if ($accion == 'ver'){
+			verConfigUsuarios();
+		}else if ($accion == 'update'){
+			updateConfigUsuario();
+		}else if ($accion == 'delete'){
+			deleteConfigUseario();
+		}
+
+	}
+
+
+	function crearConfigUsuario(){
+		/* Proteccion de Datos */
+		$params = array(
+			':usuario' => $_POST['usuario'],
+			':piel' => $_POST['piel'],
+			':respuestas' => $_POST['respuestas'],
+			);
+
+		/* Preparamos el query apartir del array $params*/
+		$query = 'INSERT INTO Configusuarios
+					(usuario, piel, respuestas) 
+				VALUES 
+                    (:usuario,:piel,:respuestas)';
+		/* Ejecutamos el query con los parametros */
+		$result = excuteQuery("Usuarios","", $query, $params);
+		if ($result > 0){
+			header('Location: viewConfigusuarios.php?result=true');
+		}else{
+			header('Location: addConfigusuarios.php?result=false');
+		}
+	}					
+
+
+	function verConfigUsuarios (){
+		$query = "SELECT * FROM Configusuarios";
+		$result = newQuery("Usuarios", "", $query);
+		if ($result != false || $result > 0){
+			foreach ($result as $value) {
+				echo "<tr>";
+				echo "    <td>".$value['idconfigusuarios']."</td>";
+				echo "    <td>".$value['usuario']."</td>";
+				echo "    <td>".$value['piel']."</td>";
+				echo "    <td>".$value['respuestas']."</td>";
+				echo "</tr>";
+			}
+		}else{
+			echo "No se encontraron resultados";
+		}
+	}
+
+	function getConfigUsuarios($id){
+		$query = "SELECT * FROM rConfigUsuarios WHERE idconfigusuarios = '".$id."'";
+		$result = newQuery("Usuarios", "", $query);
+		if ($result != false || $result > 0){
+			foreach ($result as $value) {
+				return $value;
+			}
+		}else{
+			return false;
+		}
+	}
+
+	function updateConfigUsuario (){
+
+		/* Proteccion de Datos */
+		$params = array(
+			':idconfigusuarios' => $_SESSION['idconfigusuarios'],
+			':usuario' => $_POST['usuario'],
+			':piel' => $_POST['piel'],
+			':respuestas' => $_POST['respuestas']
+		);
+
+		/* Preparamos el query apartir del array $params*/
+		$query ='UPDATE Configusuarios SET
+					usuario = :usuario,
+					piel = :piel,
+					respuestas = :respuestas
+			     WHERE idconfigusuarios = :idconfigusuarios;
+				';
+
+		$result = excuteQuery("Usuarios", "", $query, $params);
+		if ($result > 0){
+			unset($_SESSION['idconfigusuarios']);
+			$_SESSION['idconfigusuarios'] = NULL;
+			header('Location: viewConfigUsuario.php?result=true');
+		}else{
+			header('Location: editConfigUsuario.php?result=false');
+		}
+	}
+
+	function deleteConfigUseario (){
+
+		$idUser = $_GET['id'];
+
+		/* Proteccion de Datos */
+		$params = array(
+			':id' => $_GET['id'],
+		);
+
+		/* Preparamos el query apartir del array $params*/
+		$query ='DELETE FROM ConfigUseario
+				 WHERE idconfigusuarios = :id;';
+
+		$result = excuteQuery("Usuarios", "", $query, $params);
+		if ($result > 0){
+			header('Location: viewConfigUsuario.php?result=true');
+		}else{
+			header('Location: viewConfigUsuario.php?result=false');
+		}
+	}
+
+?>
